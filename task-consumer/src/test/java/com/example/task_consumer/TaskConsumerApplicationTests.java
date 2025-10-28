465e1eb8beefdfa72033bc424056283a49f61006
Sasikiran-31 <kirankumar.2001@yahoo.in>
2025-10-28 13:19:05 -0400

    The Consumer with the event listener
    This contains the endpoint for the producer that listens to the events to get published, the script for docker compose is included too.

---
+++ task-consumer/src/test/java/com/example/task_consumer/TaskConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
