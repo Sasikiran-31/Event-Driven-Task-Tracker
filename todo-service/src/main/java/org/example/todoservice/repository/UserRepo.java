33c9739ef2938ce84413fd6466c8723556c965e8
Sasikiran-31 <kirankumar.2001@yahoo.in>
2026-01-11 20:30:03 -0500

    Added Security Configuration

--- todo-service/src/main/java/org/example/todoservice/repository/UserRepo.java
+++ todo-service/src/main/java/org/example/todoservice/repository/UserRepo.java
@@ -8,5 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepo extends JpaRepository<User, Integer>{
 
     void deleteUserByUsername(String username);
+    User findbyUsername(String username);
 
 }
